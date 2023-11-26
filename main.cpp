176a7ae44f5670ce314522b7014e04cf936e47a5
spiritEcosse <shevchenkcoigor@gmail.com>
2023-11-26 10:32:18 +0100

    copy config.json; link_directories for ubuntu; drogon False: BUILD_EXAMPLES, BUILD_CTL, BUILD_TESTING; cmake_minimum_required 3.22; main.cpp add variables

--- main.cpp
+++ main.cpp
@@ -2,11 +2,34 @@
 
 using namespace drogon;
 
+bool getenv(const char *name, std::string &env)
+{
+    const char *ret = getenv(name);
+    if (ret) env = std::string(ret);
+    return !!ret;
+}
+
 int main()
 {
-    drogon::app().loadConfigFile("/Users/ihor/projects/foxy_server/config.json");
+    std::string config_app_path;
+    if (!getenv("CONFIG_APP_PATH", config_app_path)){
+        throw std::invalid_argument("CONFIG_APP_PATH is not set");
+    }
+
+    std::string env;
+
+    if (!getenv("ENV", env)){
+        throw std::invalid_argument("ENV is not set");
+    }
+    std::string foxy_client;
+
+    if (!getenv("FOXY_CLIENT", foxy_client)){
+        throw std::invalid_argument("FOXY_CLIENT is not set");
+    }
+
+    drogon::app().loadConfigFile(config_app_path);
     app().registerHandler("/test?username={name}",
-                          [](const HttpRequestPtr& req,
+                          []([[maybe_unused]] const HttpRequestPtr& req,
                              std::function<void (const HttpResponsePtr &)> &&callback,
                              const std::string &name)
                           {
@@ -17,10 +40,15 @@ int main()
                               callback(resp);
                           });
     app().registerPostHandlingAdvice(
-        [](const drogon::HttpRequestPtr &req, const drogon::HttpResponsePtr &resp) {
-            //LOG_DEBUG << "postHandling1";
-            resp->addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        [foxy_client]([[maybe_unused]] const drogon::HttpRequestPtr &req, const drogon::HttpResponsePtr &resp) {
+            resp->addHeader("Access-Control-Allow-Origin", foxy_client);
         });
-    app().setThreadNum(16);
-    app().addListener("127.0.0.1", 8848).run();
+    app().setThreadNum(std::thread::hardware_concurrency() + 2);
+    std::string http_port;
+    if (!getenv("FOXY_HTTP_PORT", http_port)){
+        throw std::invalid_argument("FOXY_HTTP_PORT is not set");
+    }
+    std::string host = env == "prod" ? "0.0.0.0" : "127.0.0.1";
+    app().addListener(host, static_cast<uint16_t>(std::stoi(http_port))).run();
+    std::cout << "server started" << std::endl;
 }
