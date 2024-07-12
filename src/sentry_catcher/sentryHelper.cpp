86d55b1d18bf05d73d28f62c0c10dc3ffa0c5372
spiritEcosse <shevchenkcoigor@gmail.com>
2024-07-12 19:20:39 +0200

    Add sentry_catcher include and refactor sentryHelper files

--- src/sentry_catcher/sentryHelper.cpp
+++ src/sentry_catcher/sentryHelper.cpp
@@ -1,7 +1,6 @@
-#include <sentry.h>
-#include <string>
-#include <drogon/drogon.h>
+#include "sentryHelper.h"
 
+// Definition moved from the header file
 void sentryHelper(const std::string& error, const std::string& logger) {
     LOG_ERROR << logger << ", " << error;
     sentry_capture_event(sentry_value_new_message_event(
