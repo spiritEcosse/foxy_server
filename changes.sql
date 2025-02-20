257258c7be704fcc830882b913a04d6339e1fbb0
spiritEcosse <shevchenkcoigor@gmail.com>
2025-02-20 13:38:46 +0100

    Update YouTube URL format to public watch link
    Replaced the YouTube base URL with the public "watch" link instead of the "studio" link. Removed unnecessary logic for appending '/edit' to the YouTube URLs, simplifying the URL generation process.

--- changes.sql
+++ changes.sql
@@ -6,15 +6,10 @@ DECLARE
 BEGIN
     CASE social_media_title
         WHEN 'Twitter' THEN base_url := 'https://x.com/faithfishart/status/';
-        WHEN 'YouTube' THEN base_url := 'https://studio.youtube.com/video/';
+        WHEN 'YouTube' THEN base_url := 'https://www.youtube.com/watch?v=';
         WHEN 'Pinterest' THEN base_url := 'https://pinterest.com/pin/';
         ELSE base_url := ''; -- Default or error case
         END CASE;
-
-    -- For YouTube, add '/edit' after the video ID
-    IF social_media_title = 'YouTube' THEN
-        RETURN base_url || external_id || '/edit';
-    END IF;
 
     RETURN base_url || external_id;
 END;
